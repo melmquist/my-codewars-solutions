5079600e3afaac513b33558de19f8faa06be1565
Matthew Elmquist <mattelmquist@gmail.com>
2017-05-23 20:22:48 -0400

    m

--- Seating arrangement at a Polyglot Summit/ft.js
+++ Seating arrangement at a Polyglot Summit/ft.js
@@ -6,7 +6,7 @@ arrangeSeating("AAAABBCC")
 
 function arrangeSeating(input) {
   var index = {};
-  var output = '';
+  var output;
 
   for (var i = 0; i < input.length; i++) {
     if (index.hasOwnProperty(input[i])){
@@ -16,30 +16,23 @@ function arrangeSeating(input) {
     }
   }
 
-  console.log("BEFORE");
-  console.log(index);
-  console.log(Object.keys(index));
+  // while (Object.keys(index).length > 0) {
+  //   for (var variable in index) {
+  //     if (index.hasOwnProperty(variable)) {
+  //       //loop through object
+  //         //if prop[val] is greater than 0, add that prop to output, and decrement
+  //         //else delete the prop
+  //       if (index[variable] > 0) {
+  //         output+= variable;
+  //         index[variable]--;
+  //       } else {
+  //         delete index[variable];
+  //       }
+  //     }
+  //   }
+  // }
 
-  while (Object.keys(index).length > 0) {
-    for (var variable in index) {
-      if (index.hasOwnProperty(variable)) {
-        //loop through object
-          //if prop[val] is greater than 0, add that prop to output, and decrement
-          //else delete the prop
-        if (index[variable] > 0) {
-          output+= variable;
-          index[variable]--;
-        } else {
-          delete index[variable];
-        }
-      }
-    }
-  }
 
-  console.log("AFTER");
-  console.log(index);
-  console.log(Object.keys(index));
-  console.log(output);
 
 
 
