c731c6252052a2060bda8a9e428a7081405548e1
Matthew Elmquist <mattelmquist@gmail.com>
2017-10-27 19:03:44 -0400

    dr

--- DirectionsReduction/3.js
+++ DirectionsReduction/3.js
@@ -4,6 +4,10 @@ go through, look at each pair.
 when it encounters a pair, take it out and call itself again with new array pair taken out
 Base Case if go through without oppFound being switched to true, then return the array
 
+
+
+
+
 */ 
 
 const dirReduce = (arr) => {
@@ -11,37 +15,32 @@ const dirReduce = (arr) => {
     if (arr.length < 2) {
         return arr;
     }
-    
-    
-    let oppFound = false;
-    
-    let cache = arr[0];
+    let holdArray = []
+  
+    for (i = 0; i <= arr.length - 1; i++) {
+        console.log("holdArray FOR LOOP: ", holdArray)
 
-    for (i = 1; i <= arr.length - 1; i++) {
-        let current = arr[i];
-
-        // console.log('pairs: ', cache, current)
-
-        if(cache === "NORTH" && current === "SOUTH"){
-            console.log("OPP FOUND", cache, current)
-        } else if(cache === "SOUTH" && current === "NORTH"){
-            console.log("OPP FOUND", cache, current)
-        } else if(cache === "EAST" && current === "WEST"){
-            console.log("OPP FOUND", cache, current)
-        } else if(cache === "WEST" && current === "EAST"){
-            console.log("OPP FOUND", cache, current)
+        if(arr[i] === "NORTH" && arr[i-1] === "SOUTH"){
+            holdArray.pop();
+        } else if(arr[i] === "SOUTH" && arr[i+1] === "NORTH"){
+            holdArray.pop();
+        } else if(arr[i] === "EAST" && arr[i+1] === "WEST"){
+            holdArray.pop();
+        } else if(arr[i] === "WEST" && arr[i+1] === "EAST"){
+            holdArray.pop();
+        } else {
+            holdArray.push(arr[i])
         }
-
-        cache = current;
-        
     }
 
-    // console.log("OUTPUT ARR: ", outputArr)
+    console.log("holdArray ARR: ", holdArray)
 
 }
 
 let test1 = ["NORTH", "SOUTH", "EAST", "WEST"];
 let test2 = ["NORTH","NORTH","NORTH", "WEST"];
 
-dirReduce(test1)
+let test3 = ["NORTH", "NORTH", "EAST", "WEST", "NORTH", "NORTH", "SOUTH"];
+let test4 = ["NORTH", "NORTH", "EAST", "WEST", "NORTH", "NORTH"];
+dirReduce(test3)
 // dirReduce(test2)
