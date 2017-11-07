c14c0f730516655f7462b041c8ad5b377ca0e7aa
Matthew Elmquist <mattelmquist@gmail.com>
2017-11-07 12:21:12 -0500

    m

---
+++ recoverSecretStringRandomTriplets/1.js
@@ -0,0 +1,11 @@
+/*
+SOCRATIC APPROACH
+
+1) define success condition
+2) define/explore key phrases
+3) match actions to actionable key phrases
+4) redefine success conditions
+5) repeat 2-4 until all key phrases are actionable 
+6) create strategy from actions
+*/
+
